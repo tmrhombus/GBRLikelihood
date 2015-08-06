6813cb7a1c8363358b15e4cf2e9f9fda9045c976
Thomas <tperry@cern.ch>
2015-08-06 03:31:30 -0500

    cleaned up

--- macros/examples/.rootlogon.C
+++ macros/examples/.rootlogon.C
@@ -1,23 +1,13 @@
 {
-
   {
-                                                                                                                                                                                                                                                                              
-    TString libstr(Form("%s/lib/%s/%s",                                                                                                                                                                                                                                       
-                      gSystem->Getenv("CMSSW_BASE"),                                                                                                                                                                                                                          
-                      gSystem->Getenv("SCRAM_ARCH"),                                                                                                                                                                                                                          
-                      "libHiggsAnalysisGBRLikelihood.so"));                                                                                                                                                                                                                   
-                                                                                                                                                                                                                                                                              
-    gSystem->Load(libstr);                                                                                                                                                                                                                                                    
-                                                                                                                                                                                                                                                                              
-  }                                                                                                                                                                                                                                                                           
-                                                                                                                                                                                                                                                                              
-                                                                                                                                                                                                                                                                              
-  gInterpreter->AddIncludePath((TString(":")+TString(gSystem->Getenv("ROOFITSYS"))+                                                                                                                                                                                           
-                                TString("/include")).Data());                                                                                                                                                                                                                 
-                                                                                                                                                                                                                                                                              
-                                                                                                                                                                                                                                                                              
-  gSystem->AddIncludePath("-I$CMSSW_BASE/src/HiggsAnalysis/GBRLikelihood/interface");                                                                                                                                                                                         
-  gSystem->AddIncludePath("-I$ROOFITSYS/include");                                                                                                                                                                                                                            
-                                                                                                                                                                                                                                                                              
-                                                                                                                                                                                                                                                                              
+    TString libstr(Form("%s/lib/%s/%s",
+     gSystem->Getenv("CMSSW_BASE"),
+     gSystem->Getenv("SCRAM_ARCH"),
+     "libHiggsAnalysisGBRLikelihood.so"));
+    gSystem->Load(libstr);                                                                            
+  }                                                                                                   
+  gInterpreter->AddIncludePath((TString(":")+TString(gSystem->Getenv("ROOFITSYS"))+                   
+                                TString("/include")).Data());                                         
+  gSystem->AddIncludePath("-I$CMSSW_BASE/src/HiggsAnalysis/GBRLikelihood/interface");                 
+  gSystem->AddIncludePath("-I$ROOFITSYS/include");                                                    
 }
